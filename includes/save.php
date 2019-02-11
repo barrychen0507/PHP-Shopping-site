ff45a4438d5b692b09fd407b191abf4838c4840d
barrychen0507 <36660571+barrychen0507@users.noreply.github.com>
2019-02-11 13:28:43 +0530

    Changes
    I have made some changes

--- includes/save.php
+++
@@ -1,4 +0,0 @@
-<?php
-include ('function.php');
-config_save('DB_NAME', 'hellomydudes', './configs/config.ini');
-?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
+//error_reporting(0);
+include('includes/function.php');
 session_start();
-
+echo "This is the home page, which I have not yet design yet.<br>";
+config_save('DB_NAME', 'THISISATEST', 'includes/configs/config.ini');
+?>
