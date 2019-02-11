695d77e602c9e5b88c7245224bb935bf2386757c
barrychen0507 <36660571+barrychen0507@users.noreply.github.com>
2019-02-11 14:41:49 +0530

    Update function.php

--- includes/function.php
+++ includes/function.php
@@ -3,8 +3,7 @@
 require ('class.php');
 function config_save($config_name, $config_value, $file){
     $config = new ConfigFileManager($file);  //opening file config.ini from configs directory  
-    //echo $config->DB_NAME;  //read  "Title" value which is equal to  "My App"  
     $config->$config_name = $config_value;  //changing value of "Title"  
-    echo $config->$config_name;
+    echo $config->$config_name; //echo the value after changing, this is not neccessary
     $config->save();  //save changes to config.ini easily
 }
